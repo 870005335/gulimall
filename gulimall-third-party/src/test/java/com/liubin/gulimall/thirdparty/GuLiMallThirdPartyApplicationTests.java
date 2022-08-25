10c6eae161c654a1b1f7101169ee06e6f3f0816d
maktub <870005335@qq.com>
2022-08-25 17:32:05 +0800

    第三方服务-OSS文件上传

---
+++ gulimall-third-party/src/test/java/com/liubin/gulimall/thirdparty/GuLiMallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liubin.gulimall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuLiMallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
